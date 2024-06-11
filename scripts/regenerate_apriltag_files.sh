516ea9ce96bb9bd3464b094c71ae7d1daef54712
Christian Rauch <Rauch.Christian@gmx.de>
2024-06-11 22:23:03 +0200

    YAML exporter

--- scripts/regenerate_apriltag_files.sh
+++ scripts/regenerate_apriltag_files.sh
@@ -3,15 +3,15 @@ set -e
 
 ant
 # Names (without first character, t/T).
-NAMES=(ag16h5 ag25h9 ag36h11 agCircle21h7 agCircle49h12 agCustom48h12 agStandard41h12 agStandard52h13)
+NAMES=(16h5 25h9 36h10 36h11 Circle21h7 Circle49h12 Custom48h12 Standard41h12 Standard52h13)
 for name in ${NAMES[*]}; do
-    java -cp april.jar april.tag.TagToC april.tag.T$name
+    java -cp april.jar april.tag.TagToYAML april.tag.Tag$name
 
-    cat scripts/copyright.txt > ../apriltag/t$name.c
-    cat t$name.c >> ../apriltag/t$name.c
-    rm t$name.c
+    # cat scripts/copyright.txt > ../apriltag/t$name.c
+    # cat t$name.c >> ../apriltag/t$name.c
+    # rm t$name.c
 
-    cat scripts/copyright.txt > ../apriltag/t$name.h
-    cat t$name.h >> ../apriltag/t$name.h
-    rm t$name.h
+    # cat scripts/copyright.txt > ../apriltag/t$name.h
+    # cat t$name.h >> ../apriltag/t$name.h
+    # rm t$name.h
 done
